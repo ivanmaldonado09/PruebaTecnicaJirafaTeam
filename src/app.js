3262fcf43c57d8fa23a2141ba54126b336503491
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-03-25 17:52:09 -0300

    Creacion del front end
    creo la carpeta public donde ira mi front end, es decir mi .html, mi .js y .css

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import peliculaRuta from './rutas/peliculaRuta.js';
-
 const app = express(); 
 
 app.use(express.json());
 
+
 app.use('/peliculas', peliculaRuta);
+
 
 export default app;
