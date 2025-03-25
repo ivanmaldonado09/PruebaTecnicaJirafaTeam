0526b932861787dab771dc6b5dbf9e5aa85eefb5
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-03-25 18:58:39 -0300

    Agrego logica de recomendaciones
    Agrego la logica para traer 4 recomendaciones fijas (incluyendo la de Gladiator)

--- src/public/script.js
+++ src/public/script.js
@@ -27,3 +27,39 @@ document.getElementById('botonBuscar').addEventListener('click', async () => {
         resultadosDiv.appendChild(peliculaElement);
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const recomendacionesDiv = document.getElementById("recomendaciones");
+    const peliculas = [
+        "https://www.omdbapi.com/?t=Gladiator&apikey=717322b7",
+        "https://www.omdbapi.com/?t=spider+man&apikey=717322b7",
+        "https://www.omdbapi.com/?t=batman&apikey=717322b7",
+        "https://www.omdbapi.com/?t=iron+man&apikey=717322b7"
+    ];
+
+    const cargarPeliculas = async () => {
+
+        for (const url of peliculas) {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+
+                if (data.Response === "True") {
+                    const img = document.createElement("img");
+                    img.src = data.Poster;
+                    img.alt = data.Title;
+                    img.style.width = "150px";
+                    img.style.margin = "10px";
+                    img.style.borderRadius = "10px";
+
+                    recomendacionesDiv.appendChild(img);
+                }
+            } catch (error) {
+                console.error("Error al cargar imagenes de recomendaciones s", error);
+            }
+        }
+    };
+
+    cargarPeliculas();
+});
