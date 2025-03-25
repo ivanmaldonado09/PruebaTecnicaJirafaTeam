1ff323d1fe3d281aefed23cac0bc38849f32f64e
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-03-25 18:34:06 -0300

    Cambio en el controlador
    En la documentacion de la api al buscar con la letra "t" se busca una sola pelicula por titulo. Con la "s" hace una busqueda y nos devuelve un arreglo con las peliculas que encontro

--- src/controladores/peliculaControlador.js
+++ src/controladores/peliculaControlador.js
@@ -7,7 +7,7 @@ const api_url = 'http://www.omdbapi.com/';
 export const buscarPelicula = async (req, res) => {
     try {
         const {titulo} = req.query;
-        const response = await axios.get(`${api_url}?apikey=${api_key}&t=${titulo}`);
+        const response = await axios.get(`${api_url}?apikey=${api_key}&s=${titulo}`);
         res.json(response.data);
     } catch (error) {
         res.status(500).json({error: error.message});
