6fd9e31ad3fb08b66853c8c37a825fc35b302888
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-03-25 16:39:50 -0300

    Reapply "creacion de la carpeta rutas y app.js"
    This reverts commit ad41c17f37e7dfed9eb5226c44f02be54f14e346.

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send("Hola Mundo");
-    }
-);
+import app from './src/app.js';
 
 app.listen(3000, () => {
     console.log('servidor funcionado en el puerto 3000');
