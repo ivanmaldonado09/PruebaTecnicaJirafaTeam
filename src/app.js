f35b39062c3a6f3654c3767519ed7f0a525173b1
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-03-25 17:57:17 -0300

    Configuracion del index.html
    Configuro mi app.js para poder renderizar mis html, utlilizando los modulos path y url

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import peliculaRuta from './rutas/peliculaRuta.js';
+import {join, dirname} from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express(); 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public/index.html'));
+}
+);
 
 app.use(express.json());
-
 
 app.use('/peliculas', peliculaRuta);
 
